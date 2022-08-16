3605fbe5050aa437385c987d3a987adc9e73057a
davidnatanel <85590345+davidnatanel@users.noreply.github.com>
2022-08-16 20:04:38 -0300

    make it better

--- index.js
+++ index.js
@@ -12,8 +12,19 @@ const app= express();
 require("dotenv").config()
 
 
-app.use(cors());
+app.use(cors({
+  origin:"*",
+  credential:true
+}));
 app.use(express.json());
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
 
 app.use("/api/auth",userRoutes);
 app.use("/api/messages",messageRoute);
@@ -37,7 +48,7 @@ const server = app.listen(process.env.PORT, ()=>{
 //socket
 const io = socket(server,{
     cors:{
-      origin:"http://localhost:3000",
+      origin:"*",
           credential:true
     }
 })
