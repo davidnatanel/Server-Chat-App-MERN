3fcdbe94c27a31a791b6abe39ac4c68002f0bd10
davidnatanel <85590345+davidnatanel@users.noreply.github.com>
2022-08-16 20:06:09 -0300

    make it better

--- index.js
+++ index.js
@@ -18,13 +18,7 @@ app.use(cors({
 }));
 app.use(express.json());
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  next();
-});
+
 
 app.use("/api/auth",userRoutes);
 app.use("/api/messages",messageRoute);
